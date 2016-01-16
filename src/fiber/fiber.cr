c2a874bd315df01316e416e675ec5d7a5aeea3ed
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-01-16 20:47:46 -0300

    Use ConcurrencyKit "big reader" locks to synchronize the GC during context switches. Set GC stackbottom only during garbage collection.

--- src/fiber/fiber.cr
+++ src/fiber/fiber.cr
@@ -4,6 +4,8 @@ fun get_stack_top : Void*
   pointerof(dummy) as Void*
 end
 
+require "ck/lib_ck"
+
 class Fiber
   STACK_SIZE = 8 * 1024 * 1024
 
@@ -11,6 +13,12 @@ class Fiber
   @@last_fiber = nil
   @@stack_pool = [] of Void*
   @@fiber_list_mutex = Mutex.new
+  @thread : Void*
+
+  # @@gc_lock = LibCK.rwlock_init
+  @@gc_lock = LibCK.brlock_init
+  @[ThreadLocal]
+  @@gc_lock_reader = LibCK.brlock_reader_init
 
   @stack : Void*
   @resume_event : Event::Event?
@@ -21,6 +29,7 @@ class Fiber
   protected property prev_fiber : Fiber?
 
   def initialize(&@proc : ->)
+    @thread = Pointer(Void).null
     @stack = Fiber.allocate_stack
     @stack_bottom = @stack + STACK_SIZE
     fiber_main = ->(f : Fiber) { f.run }
@@ -61,9 +70,12 @@ class Fiber
 
   def initialize
     @proc = Fiber.proc { }
+    @thread = LibPThread.self as Void*
     @stack = Pointer(Void).null
     @stack_top = get_stack_top
     @stack_bottom = LibGC.get_stackbottom
+
+    Fiber.gc_register_thread
 
     @@fiber_list_mutex.synchronize do
       if last_fiber = @@last_fiber
@@ -98,7 +110,7 @@ class Fiber
   end
 
   def run
-    GC.enable
+    Fiber.gc_read_unlock
     @proc.call
     @@stack_pool << @stack
 
@@ -124,6 +136,30 @@ class Fiber
     end
 
     Scheduler.reschedule
+  end
+
+  protected def self.gc_register_thread
+    LibCK.brlock_read_register pointerof(@@gc_lock), pointerof(@@gc_lock_reader)
+  end
+
+  protected def self.gc_read_lock
+    # LibCK.rwlock_read_lock pointerof(@@gc_lock)
+    LibCK.brlock_read_lock pointerof(@@gc_lock), pointerof(@@gc_lock_reader)
+  end
+
+  protected def self.gc_read_unlock
+    # LibCK.rwlock_read_unlock pointerof(@@gc_lock)
+    LibCK.brlock_read_unlock pointerof(@@gc_lock_reader)
+  end
+
+  protected def self.gc_write_lock
+    # LibCK.rwlock_write_lock pointerof(@@gc_lock)
+    LibCK.brlock_write_lock pointerof(@@gc_lock)
+  end
+
+  protected def self.gc_write_unlock
+    # LibCK.rwlock_write_unlock pointerof(@@gc_lock)
+    LibCK.brlock_write_unlock pointerof(@@gc_lock)
   end
 
   @[NoInline]
@@ -164,12 +200,19 @@ class Fiber
     end
   end
 
+  protected def thread=(@thread)
+  end
+
   def resume
-    GC.disable
+    Fiber.gc_read_lock
     current, @@current = @@current, self
-    LibGC.set_stackbottom @stack_bottom
+
+    # LibGC.set_stackbottom LibPThread.self as Void*, @stack_bottom
+    current.thread = Pointer(Void).null
+    self.thread = LibPThread.self as Void*
     Fiber.switch_stacks(pointerof(current.@stack_top), pointerof(@stack_top))
-    GC.enable
+
+    Fiber.gc_read_unlock
   end
 
   def sleep(time)
@@ -218,14 +261,25 @@ class Fiber
 
   @@prev_push_other_roots = LibGC.get_push_other_roots
 
+  LibGC.set_start_callback ->do
+    Fiber.gc_write_lock
+  end
+
   # This will push all fibers stacks whenever the GC wants to collect some memory
   LibGC.set_push_other_roots ->do
-    @@prev_push_other_roots.call
-
     fiber = @@first_fiber
     while fiber
-      fiber.push_gc_roots unless fiber == @@current
+      if thread = fiber.@thread
+        # LibC.printf "%lx\n", thread
+        LibGC.set_stackbottom thread, fiber.@stack_bottom
+      else
+        fiber.push_gc_roots
+      end
+
       fiber = fiber.next_fiber
     end
+
+    @@prev_push_other_roots.call
+    Fiber.gc_write_unlock
   end
 end
